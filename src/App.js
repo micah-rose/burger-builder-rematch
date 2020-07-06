c66ec2fd37e3f6e543a2474d12e2ae597d713f39
Micah Lund <micah.r.lund@gmail.com>
2020-07-06 15:56:31 -0600

    Imported Person component onto App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './burger-lrg.png';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         WELCOME TO BURGER BUILDER
       </p>
         <img src={logo} className="App-logo" alt="logo" /><br/>
+        <Person />
       </header>
     </div>
   );
