c82cb6a1f88725ca4dfc620918292981bdfef8d0
Micah Lund <micah.r.lund@gmail.com>
2020-07-13 21:34:33 -0600

    Changed App.js back to a class component to proceed with project course.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,41 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
+class App extends Component {
 
-//render(){
-
-const [ personsState, setPersonsState ] = useState({
-   
+state = {
   persons: [
     { name: "Max", age: 28},
     { name: "Manu", age: 29},
     { name: "Steph", age: 26}
   ],
   otherState: "some other value"
+}
 
-});
-
-//Example that you can call useState multiple times (for functional components only)
-const [otherState] = useState({otherState: "some other value"});
-console.log(personsState, otherState);
-
-const switchNameHandler = () => {
-    //console.log('Was clicked');
-    //DON'T DO THIS -> this.state.persons[0].name = "Maximilian";
-    setPersonsState({
-      persons: [
-        { name: "Maximilian", age: 28},
-        { name: "Manu", age: 29},
-        { name: "Steph", age: 27}
-      ]
-    })
-  }
-
+switchNameHandler = () => {
+  //console.log('Was clicked');
+  //DON'T DO THIS -> this.state.persons[0].name = "Maximilian";
+  this.setState({
+    persons: [
+      { name: "Maximilian", age: 28},
+      { name: "Manu", age: 29},
+      { name: "Steph", age: 27}
+    ]
+  })
+}
+ render(){
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </header>
     </div>
   );
 }
-//}
-
-
-// state = {
-//   persons: [
-//     { name: "Max", age: 28},
-//     { name: "Manu", age: 29},
-//     { name: "Steph", age: 26}
-//   ],
-//   otherState: "some other value"
-// }
-
-// switchNameHandler = () => {
-//   //console.log('Was clicked');
-//   //DON'T DO THIS -> this.state.persons[0].name = "Maximilian";
-//   this.setState({
-//     persons: [
-//       { name: "Maximilian", age: 28},
-//       { name: "Manu", age: 29},
-//       { name: "Steph", age: 27}
-//     ]
-//   })
-// }
+}
 
 export default App;
