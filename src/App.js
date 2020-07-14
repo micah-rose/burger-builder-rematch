8a02996306c3b0eb183f5d6b0a232269fa4061bb
Micah Lund <micah.r.lund@gmail.com>
2020-07-14 17:10:14 -0600

    Practiced some examples using bind, but it didn't render the same as shown in the tutorial.

--- src/App.js
+++ src/App.js
@@ -28,14 +28,14 @@ switchNameHandler = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}/>
         <Person 
         name={this.state.persons[1].name} 
         age={this.state.persons[1].age}
-        click = {this.switchNameHandler}>My Hobbies: Racing</Person>
+        click = {this.switchNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person>
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}/>
