bd35a116ab36367081606e0a57a4447dc5318e6a
Micah Lund <micah.r.lund@gmail.com>
2020-07-14 17:03:13 -0600

    Performed an example of passing methods through props.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ switchNameHandler = () => {
         age={this.state.persons[0].age}/>
         <Person 
         name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        age={this.state.persons[1].age}
+        click = {this.switchNameHandler}>My Hobbies: Racing</Person>
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}/>
