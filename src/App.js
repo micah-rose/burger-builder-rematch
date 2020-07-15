5b5d08fa822463ca50c36dccb42cdd5d19e3e340
Micah Lund <micah.r.lund@gmail.com>
2020-07-15 16:40:12 -0600

    Demonstrated additional practice using styled components to style Toggle Persons button.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    background-color: ${props => props.alt ? 'red' : 'green'};
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+    
+    &:hover{
+      background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+      color: black;
+    }
+`;
 
 class App extends Component {
 
@@ -58,18 +73,18 @@ deletePersonHandler = (personIndex) => {
 
  render(){
 
-  const style = {
-    backgroundColor: "green",
-    color: "white",
-    font: "inherit",
-    border: "1px solid blue",
-    padding: "8px",
-    cursor: "pointer",
-    ":hover":{
-      backgroundColor: "lightgreen",
-      color: "black"
-    }
-  };
+  // const style = {
+  //   backgroundColor: "green",
+  //   color: "white",
+  //   font: "inherit",
+  //   border: "1px solid blue",
+  //   padding: "8px",
+  //   cursor: "pointer",
+  //   ":hover":{
+  //     backgroundColor: "lightgreen",
+  //     color: "black"
+  //   }
+  // };
 
   let personCards = null;
   if (this.state.showPersons) {
@@ -86,11 +101,11 @@ deletePersonHandler = (personIndex) => {
       </div>
     );
 
-    style.backgroundColor = "red";
-    style[":hover"] = {
-      backgroundColor: "salmon",
-      color: "black"
-    }
+    // style.backgroundColor = "red";
+    // style[":hover"] = {
+    //   backgroundColor: "salmon",
+    //   color: "black"
+    // }
   }
 
   let classes = [];
@@ -104,9 +119,9 @@ deletePersonHandler = (personIndex) => {
   return (
     <div className="App">  
     <p className={classes.join(' ')}>This is really working!</p>  
-        <button 
-        onClick={this.togglePersonsHandler}
-        style={style}>Toggle Persons</button>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        Toggle Persons
+        </StyledButton>
         {personCards}
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,21 +2,18 @@ import React from 'react';
 //import "./Person.css";
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-        
-            width: 60%;
-            margin: 16px auto;
-            border: 1px solid #eee;
-            box-shadow: 0 2px 3px #ccc;
-            padding: 16px;
-            text-align: center;
-        
+const StyledDiv = styled.div`       
+        width: 60%;
+        margin: 16px auto;
+        border: 1px solid #eee;
+        box-shadow: 0 2px 3px #ccc;
+        padding: 16px;
+        text-align: center;       
 
         @media (min-width: 500px){
             .Person {
                 width: 450px;
-            }
-        
+        }      
     `;
 
 const person = (props) => {
