4136503ac1614ed4ddf8e1330ab89a671aa8afa3
Micah Lund <micah.r.lund@gmail.com>
2020-07-15 14:44:12 -0600

    Refactored nameChangedHandler function to make it more dynamic and not modify the state directly, but reassigning a copy of the persons array.

--- src/App.js
+++ src/App.js
@@ -26,13 +26,22 @@ switchNameHandler = (newName) => {
   })
 }
 
-nameChangedHandler = (event) => {
+nameChangedHandler = (event, id) => {
+const personIndex = this.state.persons.findIndex(p => {
+  return p.id === id;
+});
+
+const person = {
+  ...this.state.persons[personIndex]
+};
+
+person.name = event.target.value;
+
+const persons = [...this.state.persons];
+persons[personIndex] = person;
+
   this.setState({
-    persons: [
-      { name: "Max", age: 28},
-      { name: event.target.value, age: 29},
-      { name: "Steph", age: 27}
-    ]
+    persons: persons
   })
 }
 
@@ -66,7 +75,8 @@ deletePersonHandler = (personIndex) => {
           click={() => this.deletePersonHandler(index)}
           name={person.name} 
           age={person.age}
-          key={person.id}/>
+          key={person.id}
+          changed={(event) => this.nameChangedHandler(event, person.id)}/>
         })}
       </div>
     );
