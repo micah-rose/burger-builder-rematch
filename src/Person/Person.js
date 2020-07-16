b793e00849e5286bb9c32a5f7c69bbcb281853fa
Micah Lund <micah.r.lund@gmail.com>
2020-07-16 12:20:18 -0600

    Removed the ErrorBoundary info since it will no longer be used.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,6 @@ import React from 'react';
 import classes from "./Person.css";
 
 const person = (props) => {
-
-const rand = Math.random();
-
-if (rand > 0.7) {
-    throw new Error('Something went wrong');
-}
-
     return (
         //<div className="Person" style={style}>
         <div className={classes.Person}>
