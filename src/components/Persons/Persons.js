ed0b2cace63a8d3754c5fdb316b85c97e789e7a5
Micah Lund <micah.r.lund@gmail.com>
2020-07-17 18:42:22 -0600

    Commented out shouldComponentUpdate and imported PureComponent to get the same functionality.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
     // static getDerivedStateFromProps(props, state){
     //     console.log("PersonsJS get derived state from props");
     //     return state;
     // }
 
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("PersonsJS should component update");
-        if (nextProps.persons !== this.props.persons || 
-            nextProps.changed !== this.props.changed || 
-            nextProps.clicked !== this.props.clicked){
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log("PersonsJS should component update");
+    //     if (nextProps.persons !== this.props.persons || 
+    //         nextProps.changed !== this.props.changed || 
+    //         nextProps.clicked !== this.props.clicked){
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState){
         console.log("PersonsJS get snapshop before update");
