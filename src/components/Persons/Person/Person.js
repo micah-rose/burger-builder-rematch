995bf3a7f259a57edf7478febc39dec7a5f76582
Micah Lund <micah.r.lund@gmail.com>
2020-07-17 19:53:41 -0600

    Utilized PropTypes in Person to understand assigning types to props.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,25 +2,30 @@ import React, {Component} from 'react';
 import classes from "./Person.css";
 import Aux from '../../../hoc/Auxi';
 import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
   render(){
     console.log("PersonJS rendering...");
     return (
-        //<div className={classes.Person}>
-        <Aux>
-            <p onClick={this.props.click}>
+            <Aux>
+                <p onClick={this.props.click}>
                 I'm {this.props.name} and I am {this.props.age} years old!</p>
-            <p>{this.props.children}</p>
-            <input 
+                <p>{this.props.children}</p>
+                <input 
                 type="text" 
                 onChange={this.props.changed} 
                 value={this.props.name} />
-        </Aux>
-            )
-        //</div>
-    
+            </Aux>
+        ) 
+    }
 }
-}
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
