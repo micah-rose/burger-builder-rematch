f842631b5be3748931e8b1661df41e06d61b23c9
Micah Lund <micah.r.lund@gmail.com>
2020-07-17 19:02:54 -0600

    Replace Aux wrapper with Fragment to demonstrate that there is a built in feature within React that does the same thing as our Auxi component.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from "./Person.css";
 import Aux from '../../../hoc/Auxi';
 
@@ -7,14 +7,18 @@ class Person extends Component {
     console.log("PersonJS rendering...");
     return (
         //<div className={classes.Person}>
-        <Aux className={classes.Person}>
+        <Fragment>
             <p onClick={this.props.click}>
                 I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-        </Aux>
+            <input 
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} />
+        </Fragment>
+            )
         //</div>
-    )
+    
 }
 }
 
--- src/hoc/Auxi.js
+++ src/hoc/Auxi.js
@@ -1,4 +1,3 @@
-
 const auxi = props => props.children;
 
 export default auxi;
