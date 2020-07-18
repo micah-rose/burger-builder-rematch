d3888a8876b4500df20c8ae657db2e3c24bf5cb4
Micah Lund <micah.r.lund@gmail.com>
2020-07-17 18:59:31 -0600

    Created an auxillary component for wrapping JSX code in something other than a div.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import classes from "./Person.css";
+import Aux from '../../../hoc/Auxi';
 
 class Person extends Component {
   render(){
     console.log("PersonJS rendering...");
-    return [
+    return (
         //<div className={classes.Person}>
-            <p key="i1" 
-            onClick={this.props.click}>
-                I'm {this.props.name} and I am {this.props.age} years old!</p>,
-            <p>{this.props.children}</p>,
+        <Aux className={classes.Person}>
+            <p onClick={this.props.click}>
+                I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+        </Aux>
         //</div>
-  ];
+    )
 }
 }
 
