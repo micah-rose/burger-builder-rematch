1791fe824af94af047c527e14a5726178ae4546d
Micah Lund <micah.r.lund@gmail.com>
2020-07-17 18:53:48 -0600

    Introducecd a new way to write JSX where HTML tags are put within square brackets.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,15 @@ import classes from "./Person.css";
 class Person extends Component {
   render(){
     console.log("PersonJS rendering...");
-    return (
-        <div className={classes.Person}>
-            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-            <p>{this.props.children}</p>
+    return [
+        //<div className={classes.Person}>
+            <p key="i1" 
+            onClick={this.props.click}>
+                I'm {this.props.name} and I am {this.props.age} years old!</p>,
+            <p>{this.props.children}</p>,
             <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-        </div>
-    )
+        //</div>
+  ];
 }
 }
 
