7e9083dc22388f21b47cb2681cdb9bea3ab84957
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 17:14:41 -0600

    Mapped the ingredients objects into an array.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,15 @@ import classes from '../Burger/Burger.css';
 import Ingredients from '../Burger/Ingredients';
 
 const Burger = ( props ) => {
+
+    const transIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                return <Ingredients key={igKey + i} type={igKey} />
+                });
+        });
+
     return (
         <div className={classes.Burger}>
             <Ingredients type="bread-top" />
