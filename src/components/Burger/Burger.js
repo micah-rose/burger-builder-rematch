5c4e9481fc74dc452f542a718e8f4d6e6250150a
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 17:27:35 -0600

    Added reduce to the end of the tranIngredients array to make it possible to dynamically add or remove ingredients off our burger.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import Ingredients from '../Burger/Ingredients';
 
 const Burger = ( props ) => {
 
-    const transIngredients = Object.keys(props.ingredients)
+    let transIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                 return <Ingredients key={igKey + i} type={igKey} />
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+        
+        if(transIngredients.length === 0) {
+            transIngredients = <p>Please start adding ingredients!</p>
+        }
 
     return (
         <div className={classes.Burger}>
