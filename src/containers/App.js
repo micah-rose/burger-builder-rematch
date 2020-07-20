311d4521ec49f07bc33505b81e0f02804caf84a6
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 16:14:58 -0600

    Began to set up the layout template of our burger builder.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,15 @@
-//Copy of App.js for when I figure out how to hook/route to other components
-
-import React from 'react';
-import logo from '../assets/burger-lrg.png';
-import classes from './App.css';
-
-
-function App() {
-
-  return (
-    <div className={classes.App}>
-      <header className={classes.Header}>
-      <p>
-        WELCOME TO BURGER BUILDER
-      </p>
-        <img src={logo} className={classes.Logo} alt="logo" /><br/> 
-        <a className={classes.Link} href="www.Google.com">Click Here to Build Your Burger</a> 
-      </header>
-    </div>
-  );
+import React, { Component }from 'react';
+import Layout from '../components/Layout/Layout';
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
