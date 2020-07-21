d777f0e0ddbb96b93f2f5640c4a85e3668985194
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 18:33:24 -0600

    Changed button label to be symbols versus words.

--- src/components/Burger/BuildControls/Controls.js
+++ src/components/Burger/BuildControls/Controls.js
@@ -7,8 +7,8 @@ const Controls = ( props ) => (
         <button 
             className={classes.Less} 
             onClick={props.removed} 
-            disabled={props.disabled}>Less</button>
-        <button className={classes.More} onClick={props.added}>More</button>
+            disabled={props.disabled}>-</button>
+        <button className={classes.More} onClick={props.added}>+</button>
     </div>
 );
 
