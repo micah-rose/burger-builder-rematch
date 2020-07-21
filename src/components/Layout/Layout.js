4aa2091d66bd3bd986f056108c2607f8cddbe4c2
Micah Lund <micah.r.lund@gmail.com>
2020-07-21 14:45:31 -0600

    Imported SideDrawer into Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxi';
-import classes from '../Layout/Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import classes from '../Layout/Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = ( props ) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
