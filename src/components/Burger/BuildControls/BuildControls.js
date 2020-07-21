74c3c13f4597f2005c99519f2ee58540d3e53880
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 18:02:43 -0600

    Displayed controls on the DOM.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import classes from '../Burger/BuildControls.css';
+import classes from '../BuildControls/BuildControls.css';
+import Controls from '../BuildControls/Controls';
+
+const controllers = [
+    {label: 'Lettuce', type: 'lettuce'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
 
 const BuildControls = ( props ) => (
     <div className={classes.BuildControls}>
-
+        {controllers.map(ctrl => (
+            <Controls key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
 )
 
