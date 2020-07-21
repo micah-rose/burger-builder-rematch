f1e623c0400c3c4f171e65d8199be8e232fe08d4
Micah Lund <micah.r.lund@gmail.com>
2020-07-21 14:00:47 -0600

    Finalized logo component and rendered to DOM.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from '../Logo/Logo.css'
 
 const Logo = (props) => (
-    <div>
-        <img src={}/>
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="BurgerBuilder"/>
     </div>
 );
 
