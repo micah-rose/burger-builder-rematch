7fe04c9dd9d754f9e28e80b001e149b2eb6eab38
Micah Lund <micah.r.lund@gmail.com>
2020-07-20 18:31:03 -0600

    Created a constant variable to disabled the Less button if there are no ingredients that have been added.

--- src/components/Burger/BuildControls/Controls.js
+++ src/components/Burger/BuildControls/Controls.js
@@ -4,7 +4,10 @@ import classes from '../BuildControls/Controls.css';
 const Controls = ( props ) => (
     <div className={classes.Controls}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less} onClick={props.removed}>Less</button>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
         <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
