4f8fec3703962a30272a0297285cba77806ed6d9
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 17:38:14 -0600

    Worked through examples on how to remove interceptors.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,20 @@ import React, { Component }from 'react';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
+
+  // state = {
+  //   show: true
+  // }
+
+  // componentDidMount() {
+  //     setTimeout(() => {
+  //       this.setState({show: false})
+  //     }, 5000)
+  // }
+
+  //Used with checking for WillUnMount 
+  //{this.state.show ? <BurgerBuilder/>  : null } 
+  
   render(){
     return (
       <div>
