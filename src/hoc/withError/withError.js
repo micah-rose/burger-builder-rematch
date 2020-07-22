57122ddb459419eed8f95d6d9fdaaa08c40fb07d
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 17:29:05 -0600

    Removed axios import from withError since it wasn't being used.

--- src/hoc/withError/withError.js
+++ src/hoc/withError/withError.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxi'
-import axios from 'axios';
 
 const withError = (WrappedComponent, axios) => {
 
