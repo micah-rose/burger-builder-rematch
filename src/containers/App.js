5e911cf8e776b48d3744e1ac831ad3994e0efeba
Micah Lund <micah.r.lund@gmail.com>
2020-07-23 08:38:32 -0600

    Started developing the checkout features for after a user clicked continue when ordering a burger.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
+import Checkout from '../containers/Checkout/Checkout';
 class App extends Component {
 
   // state = {
@@ -15,12 +16,13 @@ class App extends Component {
 
   //Used with checking for WillUnMount 
   //{this.state.show ? <BurgerBuilder/>  : null } 
-  
+
   render(){
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
