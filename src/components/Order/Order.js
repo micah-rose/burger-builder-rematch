201624660735c531eb9129addd1c70833490510d
Micah Lund <micah.r.lund@gmail.com>
2020-07-23 15:05:55 -0600

    Started adding orders page.

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from '../Order/Order.css'
+
+const Order = (props) => {
+    <div className={classes.Order}>
+        <p>Ingredients: Lettuce (1)</p>
+        <p>Price: <strong>USD 5.50</strong></p>
+    </div>
+}
+
+export default Order;
