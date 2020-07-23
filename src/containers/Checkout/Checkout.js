db8b51fa12e28cc7666f197bd578b489649a3828
Micah Lund <micah.r.lund@gmail.com>
2020-07-23 13:55:41 -0600

    Finalized styling and navigation to ContactData component.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -39,6 +41,7 @@ render() {
             ingredients={this.state.ingredients}
             checkoutCancelled={this.checkoutCancelledHandler}
             checkoutContinued={this.checkoutContinuedHandler} />
+            <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
         </div>
     )
 }
