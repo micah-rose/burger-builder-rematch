0fc61849be46b18fc0952ce1716c07785415ee63
Micah Lund <micah.r.lund@gmail.com>
2020-07-24 16:57:27 -0600

    Set up handling for purchasing and updating the UI.

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -2,7 +2,8 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     orders: [],
-    loading: false
+    loading: false,
+    purchased: false
 }
 
 const reducer = (state = initialState, action) => {
@@ -21,12 +22,18 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 loading: false,
-                orders: state.orders.concat(newOrder)
+                orders: state.orders.concat(newOrder),
+                purchased: true
             };
         case actionTypes.PURCHASE_BURGER_FAILED:
             return {
                 ...state,
                 loading: false
+            };
+        case actionTypes.PURCHASE_INIT:
+            return {
+                ...state,
+                purchased: false
             };
     default: 
         return state;
