54edf0a956946d15a2b3eb091902f53e1329233a
Micah Lund <micah.r.lund@gmail.com>
2020-07-24 09:27:22 -0600

    Modified updatePurchaseState to take in the new arguments from mapStateToProps to unlock the Order Now button.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,21 +13,9 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state= {
-        totalPrice: 3,
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
-    }
-
-    componentDidMount() {
-        // axios.get('https://react-burger-builder-28ad4.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data})
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true})
-        //     })
     }
 
     updatePurchaseState (ingredients) {
@@ -39,7 +27,7 @@ class BurgerBuilder extends Component {
                 return sum + el;
             }, 0);
 
-        this.setState({purchasable: sum > 0});
+        return sum > 0;
     }
 
     
@@ -86,7 +74,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.props.onIngredientAdded}
                     ingredientRemoved={this.props.onIngredientRemoved}
                     disabled = {disabledInfo}
-                    purchasable = {this.state.purchasable}
+                    purchasable = {this.updatePurchaseState(this.props.ings)}
                     price={this.props.price}
                     ordered={this.purchaseHandler} />
             </Aux>
