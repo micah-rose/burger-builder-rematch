6ad090af8f333748578f242f75b72079836f39cd
Micah Lund <micah.r.lund@gmail.com>
2020-07-23 18:09:46 -0600

    Set up the prep for form submission handling.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,13 +9,30 @@ switch (props.elementType){
             inputElement = <input 
             className={classes.InputElement} 
             {...props.elementConfig} 
-            value={props.value}/>
+            value={props.value}
+            onChange={props.changed}/>
             break;
     case ('textarea'):
             inputElement = <textarea 
             className={classes.InputElement} 
-            {...props.elementConfig} value={props.value}/>
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed}/>
             break;
+    case('select'):
+        inputElement = (
+            <select 
+                className={classes.InputElement} 
+                value={props.value}
+                onChange={props.changed}>
+                {props.elementConfig.options.map(
+                    option => (
+                    <option key={option.value} value={option.value} >
+                        {option.displayValue}</option>
+                ))}               
+            </select>
+        )
+        break;
     default:
             inputElement = <input 
             className={classes.InputElement} 
