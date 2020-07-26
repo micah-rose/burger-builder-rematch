6c4cea2227cc9833feb5ae27e28942aa4783f22b
Micah Lund <micah.r.lund@gmail.com>
2020-07-26 15:04:18 -0600

    Added sign in logic.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,19 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAejuIPBOhTYoxmIdhdH_hI8mvyCSF6PSM';
+        if (isSignup){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:verifypassword?key=AIzaSyAejuIPBOhTYoxmIdhdH_hI8mvyCSF6PSM';
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAejuIPBOhTYoxmIdhdH_hI8mvyCSF6PSM', authData)
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
