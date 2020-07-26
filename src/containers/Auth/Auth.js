849687357de56aa615eb53323b57c7efffa4fe66
Micah Lund <micah.r.lund@gmail.com>
2020-07-26 15:47:55 -0600

    Added a spinner for after a user logs in and adjusted the error message that displays with invalid info.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from '../../containers/Auth/Auth.css';
 import * as actions from '../../store/actions/index';
 import {connect} from 'react-redux';
-
+import Spinner from '../../components/UI/Spinner/Spinner';
 class Auth extends Component {
 
     state = {
@@ -103,7 +103,7 @@ class Auth extends Component {
         })
         }
 
-        const form = formElements.map(formEl => (
+        let form = formElements.map(formEl => (
             <Input 
                 key={formEl.id}
                 elementType={formEl.config.elementType}
@@ -114,10 +114,22 @@ class Auth extends Component {
                 touched={formEl.config.touched}
                 changed={(event) => 
                     this.inputChangedHandler(event, formEl.id)} />
-        ))
+        ));
+
+        if (this.props.loading){
+            form = <Spinner />
+        }
+
+        let errorMessage = null;
+        if (this.props.error){
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
         
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                 <Button btnType="Success">SUBMIT</Button>
@@ -130,10 +142,17 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
