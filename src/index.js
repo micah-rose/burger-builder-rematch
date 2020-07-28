22ceb35710c02b634814ccd2125bd7f95be6af4b
Micah Lund <micah.r.lund@gmail.com>
2020-07-27 18:07:36 -0600

    Moved logic from the action creator for logout to the saga.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import burgerReducer from '../src/store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth'
 import createSagaMiddleware from 'redux-saga';
-import { logoutSaga } from './store/saga/auth';
+import { watchAuth } from './store/saga';
 
 const composeEnhancers = (process.env.NODE_ENV === 'development' 
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
@@ -28,7 +28,7 @@ const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk, sagaMiddleware)
 ));
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
