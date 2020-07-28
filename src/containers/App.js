2f3336dc27d038cda95fa6b8d802606ee9ec69a7
Micah Lund <micah.r.lund@gmail.com>
2020-07-28 16:55:20 -0600

    Removed async component and applied React.lazy() to App.js.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import Logout from '../containers/Auth/Logout/Logout';
 import {connect} from 'react-redux';
 import * as actions from '../store/actions/index';
-import asyncComp from '../hoc/asyncComp/asyncComp';
 
-const asyncCheckout = asyncComp(() => {
+const Checkout = React.lazy(() => {
   return import('../containers/Checkout/Checkout')
 });
 
-const asyncOrders = asyncComp(() => {
+const Orders = React.lazy(() => {
   return import('../containers/Orders/Orders')
 });
 
-const asyncAuth = asyncComp(() => {
+const Auth = React.lazy(() => {
   return import('../containers/Auth/Auth')
 });
 
@@ -26,7 +25,7 @@ const App = props => {
 
     let routes = (
       <Switch>
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" render={() => <Auth />} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -35,10 +34,10 @@ const App = props => {
     if (props.isAuthenticated){
       routes = (
         <Switch>
-        <Route path="/checkout" component={asyncCheckout} />
-        <Route path="/orders" component={asyncOrders} />
+        <Route path="/checkout" render={() => <Checkout />} />
+        <Route path="/orders" render={() => <Orders />} />
         <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" render={() => <Auth />} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
     </Switch>
@@ -48,7 +47,7 @@ const App = props => {
     return (
       <div>
         <Layout>
-          {routes}
+          <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
         </Layout>
       </div>
     );
