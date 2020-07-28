0e2dc33e3372b45871215eb774a5e9bb024af4e2
Micah Lund <micah.r.lund@gmail.com>
2020-07-27 22:22:23 -0600

    Refactored watchAuth in index.js in saga folder to use all function.

--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -1,14 +1,17 @@
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, all} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { initIngredientsSaga } from './burgerBuilder';
 import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 
 export function* watchAuth(){
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    yield all([
+     takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+     takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+     takeEvery(actionTypes.AUTH_USER, authUserSaga),
+     takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    ]);
+
 }
 
 export function* watchBurgerBuilder(){
